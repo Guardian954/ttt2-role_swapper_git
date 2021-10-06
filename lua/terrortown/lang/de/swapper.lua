41ff8f67988037ede8661bdc2038317a64c59705
Tim Goll <tim@timgoll.de>
2021-10-06 14:19:18 +0200

    roles.

--- lua/terrortown/lang/de/swapper.lua
+++ lua/terrortown/lang/de/swapper.lua
@@ -1,11 +1,11 @@
 local L = LANG.GetLanguageTableReference("de")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[SWAPPER.name] = "Wechsler"
-L["info_popup_" .. SWAPPER.name] = [[Du bist der Wechsler. Versuche zu sterben]]
-L["body_found_" .. SWAPPER.abbr] = "Er war ein Wechsler!"
-L["search_role_" .. SWAPPER.abbr] = "Diese Person war ein Wechsler!"
-L["target_" .. SWAPPER.name] = "Wechsler"
-L["ttt2_desc_" .. SWAPPER.name] = [[Der Wechsler ist eine Gauklerrolle, welche ihrem Mörder die Identität stielt. Der Mörder wird dann zum neuen Wechsler!]]
+L[roles.SWAPPER.name] = "Wechsler"
+L["info_popup_" .. roles.SWAPPER.name] = [[Du bist der Wechsler. Versuche zu sterben]]
+L["body_found_" .. roles.SWAPPER.abbr] = "Er war ein Wechsler!"
+L["search_role_" .. roles.SWAPPER.abbr] = "Diese Person war ein Wechsler!"
+L["target_" .. roles.SWAPPER.name] = "Wechsler"
+L["ttt2_desc_" .. roles.SWAPPER.name] = [[Der Wechsler ist eine Gauklerrolle, welche ihrem Mörder die Identität stielt. Der Mörder wird dann zum neuen Wechsler!]]
 
 -- OTHER ROLE LANGUAGE STRINGS
