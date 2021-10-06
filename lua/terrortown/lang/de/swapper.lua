b4840999c435e3d9de5f00fe0f605798ff581474
Tim Goll <tim@timgoll.de>
2021-10-06 12:00:48 +0200

    added german language

---
+++ lua/terrortown/lang/de/swapper.lua
@@ -0,0 +1,11 @@
+local L = LANG.GetLanguageTableReference("de")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SWAPPER.name] = "Wechsler"
+L["info_popup_" .. SWAPPER.name] = [[Du bist der Wechsler. Versuche zu sterben]]
+L["body_found_" .. SWAPPER.abbr] = "Er war ein Wechsler!"
+L["search_role_" .. SWAPPER.abbr] = "Diese Person war ein Wechsler!"
+L["target_" .. SWAPPER.name] = "Wechsler"
+L["ttt2_desc_" .. SWAPPER.name] = [[Der Wechsler ist eine Gauklerrolle, welche ihrem Mörder die Identität stielt. Der Mörder wird dann zum neuen Wechsler!]]
+
+-- OTHER ROLE LANGUAGE STRINGS
